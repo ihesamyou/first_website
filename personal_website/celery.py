1bc3f53b06f39b8c20d9790458568970f14107bb
ihosseinu <ihosseinu@gmail.com>
2022-01-09 19:16:39 +0330

    celery connections enabled

--- personal_website/celery.py
+++ personal_website/celery.py
@@ -1,8 +1,7 @@
 import os
 from celery import Celery
-
-# from django.db import connections
-# from django.db.utils import DEFAULT_DB_ALIAS, load_backend
+from django.db import connections
+from django.db.utils import DEFAULT_DB_ALIAS, load_backend
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personal_website.settings")
@@ -19,9 +18,9 @@ def debug_task(self):
     print(f"Request: {self.request!r}")
 
 
-# def create_connection(alias=DEFAULT_DB_ALIAS):
-#     connections.ensure_defaults(alias)
-#     connections.prepare_test_settings(alias)
-#     db = connections.databases[alias]
-#     backend = load_backend(db["ENGINE"])
-#     return backend.DatabaseWrapper(db, alias)
+def create_connection(alias=DEFAULT_DB_ALIAS):
+    connections.ensure_defaults(alias)
+    connections.prepare_test_settings(alias)
+    db = connections.databases[alias]
+    backend = load_backend(db["ENGINE"])
+    return backend.DatabaseWrapper(db, alias)
